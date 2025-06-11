bde9f899c8ba35fac9415f060fad9e6a8baa699b
JUNEBATCH <siddhishinde317@gmail.com>
2025-06-11 14:32:53 +0530

    chnged the console messages and link to view the websites

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let app=require("./src/app.js");
 let PORT=3100;
 
 app.listen(PORT,(req,res)=>{
-    console.log("Server Started " +PORT);
+    console.log("Server Started " +PORT +`click here to run and view the project http://localhost:${PORT}`);
 });
